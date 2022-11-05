af84d481d12f3eb3b668a38b00742c56431975b8
DanielMues <54837088+DanielMues@users.noreply.github.com>
2022-11-05 18:24:12 +0100

    add unitstats

---
+++ Collector/Assets/Scripts/UnitStats.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitStats : MonoBehaviour
+{
+    public int healthPoints;
+    public int speed;
+    public int attackdamage;
+    public int range;
+}
