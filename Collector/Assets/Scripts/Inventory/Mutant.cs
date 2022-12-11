70c5034b1f8d50ec175c8b5b463d91428e82141e
DanielMues <54837088+DanielMues@users.noreply.github.com>
2022-12-11 16:23:45 +0100

    first test

---
+++ Collector/Assets/Scripts/Inventory/Mutant.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Mutant", menuName = "Mutant/Create New Mutant")]
+public class Mutant : ScriptableObject
+{
+    public GameObject mutantPrefab;
+    public int healthPoints;
+    public float attackSpeed;
+    public float movespeed;
+    public int attackDamage;
+    public int range;
+    public int mana;
+    public string team;
+    TypeAndClassHandler.unitTyp myTyp;
+    TypeAndClassHandler.unitClass myClass;
+}
